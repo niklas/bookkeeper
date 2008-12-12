b33ba5a82caa538a869894e8a49dafcab5ae96dd
Scott Patten <spatten@Steven-Luschers-MacBook-Pro.local>
2008-12-12 11:30:20 -0800

    some models need to be loaded first in bookkeeper.rb

--- lib/bookkeeper.rb
+++ lib/bookkeeper.rb
@@ -2,6 +2,11 @@ require 'find'
 
 require File.join(File.dirname(__FILE__), 'immutable-attribute-plugin', 'init')
 
+# These ones need to be loaded first.
+require 'bookkeeper/account.rb'
+require 'bookkeeper/paypal_transaction.rb'
+require 'bookkeeper/journal.rb'
+
 # require everything in lib/
 Find.find(File.join(File.dirname(__FILE__), 'bookkeeper')) do |file|
   require file if !File.directory?(file) && File.extname(file) == '.rb'
