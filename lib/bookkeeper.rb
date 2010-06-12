1bccff1358e5d14c8ac196209ccd896d3efe2dc2
Niklas Hofer <niklas+dev@lanpartei.de>
2010-06-12 18:34:39 +0200

    repair require madness a bit

--- lib/bookkeeper.rb
+++ lib/bookkeeper.rb
@@ -1,33 +1,32 @@
 require 'acts_as_tree'
 require 'acts_as_list'
 
-require File.join(File.dirname(__FILE__), 'immutable-attribute-plugin', 'init')
+require 'immutable-attribute-plugin/init'
 
 module Bookkeeper; end
-lib = File.dirname(__FILE__)
 
+require 'bookkeeper/account'
+require 'bookkeeper/account/asset'
+require 'bookkeeper/account/expense'
+require 'bookkeeper/account/liability'
+require 'bookkeeper/account/revenue'
 
-require lib + '/bookkeeper/account'
-require lib + '/bookkeeper/account/asset'
-require lib + '/bookkeeper/account/expense'
-require lib + '/bookkeeper/account/liability'
-require lib + '/bookkeeper/account/revenue'
+require 'bookkeeper/asset_type'
+require 'bookkeeper/asset_type/cad'
+require 'bookkeeper/asset_type/usd'
+require 'bookkeeper/asset_type/eur'
 
-require lib + '/bookkeeper/asset_type'
-require lib + '/bookkeeper/asset_type/cad'
-require lib + '/bookkeeper/asset_type/usd'
+require 'bookkeeper/batch'
 
-require lib + '/bookkeeper/batch'
+require 'bookkeeper/journal'
+require 'bookkeeper/journal/bill'
+require 'bookkeeper/journal/deposit'
+require 'bookkeeper/journal/disbursement'
+require 'bookkeeper/journal/invoice'
+require 'bookkeeper/journal/transfer'
 
-require lib + '/bookkeeper/journal'
-require lib + '/bookkeeper/journal/bill'
-require lib + '/bookkeeper/journal/deposit'
-require lib + '/bookkeeper/journal/disbursement'
-require lib + '/bookkeeper/journal/invoice'
-require lib + '/bookkeeper/journal/transfer'
+require 'bookkeeper/paypal_transaction'
+require 'bookkeeper/paypal_transaction/single'
+require 'bookkeeper/paypal_transaction/masspay_subpayment'
 
-require lib + '/bookkeeper/paypal_transaction'
-require lib + '/bookkeeper/paypal_transaction/single'
-require lib + '/bookkeeper/paypal_transaction/masspay_subpayment'
-
-require lib + '/bookkeeper/posting'
+require 'bookkeeper/posting'
